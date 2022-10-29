a8fca55f4413cd276c6c0aab0850d964f1ac6d76
SharmaNidhiP <115006056+SharmaNidhiP@users.noreply.github.com>
2022-10-29 17:19:40 +0530

    Add files via upload

---
+++ ass5ques3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Employee
+{
+
+
+    static public void Main()
+    {
+
+
+        LinkedList<String> my_list = new LinkedList<String>();
+
+        // Adding elements in the LinkedList
+        // Using AddLast() method
+        my_list.AddLast("arya");
+        my_list.AddLast("ansh");
+        my_list.AddLast("anup");
+        my_list.AddLast("arush");
+        my_list.AddLast("arohi");
+        my_list.AddLast("abhay");
+
+
+        Console.WriteLine("Name of employees:");
+
+
+        foreach (string str in my_list)
+        {
+            Console.WriteLine(str);
+        }
+        Console.WriteLine("Number of employees: {0}" +my_list.Count);
+        if (my_list.Contains("Shilpa") == true)
+        {
+            Console.WriteLine("Element Found...!!");
+        }
+        else
+        {
+            Console.WriteLine("Element Not found...!!");
+        }
+
+
+    }
+}
---
+++ assi5ques2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListEmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList personList = new ArrayList();
+            personList.Add("Neha");
+            personList.Add("Narul");
+            personList.Add("Sapna");
+            personList.Add("Ravina");
+            for (int i = 0; i < personList.Count; i++)
+            {
+                string arrayItem = string.Format($"Name is {personList[i]}");
+                Console.WriteLine(arrayItem);
+            }
+            Console.Read();
+        }
+    }
+}
